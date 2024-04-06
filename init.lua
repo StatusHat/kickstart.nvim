84f2b002ed89f276793b7c2de64cbd47e0873c55
StatusHat <oysteintossegjerlow@gmail.com>
2024-04-06 17:28:06 +0200

    Add nerdfonts and colorscheme

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -740,6 +740,7 @@ require('lazy').setup({
     end,
   },
 
+  --[[
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -755,6 +756,15 @@ require('lazy').setup({
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  --]]
+
+  { -- Dracula colorscheme
+    'dracula/vim',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 
